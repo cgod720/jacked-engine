d4536b98a9e90ab0e71474a6d44843f3243e8315
carlos-godoy720 <bit.krypton@protonmail.com>
2022-04-04 16:50:34 -0400

    collection complete, ready to update info

--- utils/countAnimated.js
+++ utils/countAnimated.js
@@ -6,19 +6,31 @@ fs.readdir('/Users/carlosgodoy/Documents/projects/cryptohunkz-static-engine/buil
     if (err) {
         console.log(err)
     } else {
-        // let gifs = 0
-        // data.forEach(file => {
-        //     // if(file.slice(-3) === 'png'){
-        //         gifs++
-        //     // }
-        // })
-        // console.log(gifs)
-        const gifs = []
+        let gifs = 0
+        let pngs = 0
         data.forEach(file => {
             if(file.slice(-3) === 'gif'){
-                gifs.push(file)
+                gifs++
+            } else if(file.slice(-3) === 'png'){
+                pngs++
             }
         })
-        console.log(gifs.length)
+        console.log(gifs)
+        console.log(pngs)
+
+
+        // const gifs = []
+        // data.forEach(file => {
+        //     if(file.slice(-3) === 'gif'){
+        //         gifs.push(file)
+        //     }
+        // })
+        // for (let i = 0; i < gifs.length; i++) {
+        //     const thePath = `${base}/${gifs[i]}`
+        //     // console.log(thePath.slice(0, thePath.length - 3))
+        //     if(fs.existsSync(`${thePath.slice(0, thePath.length - 3)}png`)){
+        //         console.log(`${thePath.slice(0, thePath.length - 3)}png`)
+        //     }
+        // }
     }
 })
